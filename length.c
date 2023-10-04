1d2417a5f5891301c3b03dec0516b4d42a2d9c62
DJ383295 <143648589+DJ383295@users.noreply.github.com>
2023-10-04 19:00:11 +0000

    Wed, Oct 4, 2023, 7:00 PM +00:00

--- length.c
+++ length.c
@@ -10,13 +10,16 @@ int main(void)
     // Prompt for user's name
     string name = get_string("Name: ");
 
+
+    printf("%i\n", n);
+}
+
+int string_length(string s)
+{
     // Count number of characters up until '\0' (aka NUL)
     int n = 0;
     while (name[n] != '\0')
     {
         n++;
     }
-    printf("%i\n", n);
 }
-
-int string_length(string s)
