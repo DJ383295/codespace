d3f56cddee777b025a0fb73ee794e3c99d601235
DJ383295 <143648589+DJ383295@users.noreply.github.com>
2023-10-04 19:18:25 +0000

    Wed, Oct 4, 2023, 7:18 PM +00:00

--- string.c
+++ string.c
@@ -8,7 +8,7 @@ int main(void)
 {
     string s = get_string("Input:  ");
     printf("Output: ");
-    for (int i = 0; n = strlen(s); i < strlen(s); i++)
+    for (int i = 0; n = strlen(s); i < n; i++)
     {
         printf("%c", s[i]);
     }
