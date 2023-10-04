fdd004f00e73e384bbc6aa000f66e6b7518e3947
DJ383295 <143648589+DJ383295@users.noreply.github.com>
2023-10-04 18:44:57 +0000

    Wed, Oct 4, 2023, 6:44 PM +00:00

--- hibye.c
+++ hibye.c
@@ -5,9 +5,10 @@
 
 int main(void)
 {
-    string s = "HI!";
-    string t = "BYE!";
+    string words[2];
+    words[0] = "HI!";
+    words[1] = "BYE!";
 
-    printf("%s\n", s);
-    printf("%s\n", t);
+    printf("%c %c %c \n", words[0]);
+    printf("%c %c %c %c \n", words[1]);
 }
